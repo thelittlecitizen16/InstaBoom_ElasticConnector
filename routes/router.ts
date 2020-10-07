6cd2f8133eb5c4265c00c52c8377f65d7a500b3e
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-10-07 10:59:45 +0300

    add error handle

--- routes/router.ts
+++ routes/router.ts
@@ -4,29 +4,39 @@ import { schemasMiddleware } from './schemas'
 import { CreateRequestBody } from '../queryControllers/elasticQueryConverter'
 import { SearchElasticQuery, RemoveTtlFieldFromElastic } from '../elasticControllers/elasticConnector'
 
+
+function CreateError(error : any)
+{
+    return {"error": error}
+}
+
 router.post('/search', schemasMiddleware(), (req, res) => {
     try {
         SearchElasticQuery(CreateRequestBody(req.body))
             .then(function (resp: any) {
                 res.json(resp.hits.hits);
             }, function (err: { message: any; }) {
-                res.status(404).json(err.message);
+                res.status(404).json(CreateError(err.message))
             });
     } catch (error) {
-        res.status(500).send();
+        res.status(500).json(CreateError(error));
     }
 });
 
 router.post('/archive/:id', (req, res) => {
     try {
         RemoveTtlFieldFromElastic(req.params.id).then(function (resp: any) {
-            res.status(201).send(resp);
+            if(resp.updated > 0){
+                res.status(201).send(resp);
+            }else{
+                res.status(404).json(CreateError(`Id Not Found`));
+            }
         }, function (err: { message: any; }) {
-            res.status(404).json(err.message);
+            res.status(404).json(CreateError(err.message));
         });
 
     } catch (error) {
-        res.status(500).send();
+        res.status(500).json(CreateError(error));
     }
 });
 
